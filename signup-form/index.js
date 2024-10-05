88b86ded8174063f49421d3640b489d3babaee05
Ben Xiao <benssiao@gmail.com>
2024-10-05 18:53:55 +0800

    finished first project

---
+++ signup-form/index.js
@@ -0,0 +1,23 @@
+
+
+function validatePassword() {
+    const password = document.querySelector("#password");
+    const confirm_password = document.querySelector("confirm_password");
+
+
+}
+
+function main() {
+    const validPassword = false;
+    const password = document.querySelector("#password");
+    const confirm_password = document.querySelector("confirm_password");
+    confirm_password.addEventListener("keypress", validatePassword);
+
+    const submitButton = document.querySelector("#submit_button");
+    submitButton.addEventListener("onclick", )
+    
+
+
+}
+
+main();
